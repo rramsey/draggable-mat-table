e2e2aed43b2c0d455bd49274f2809fbbde61b440
Robert Ramsey <robert-ramsey@uiowa.edu>
2023-04-21 11:58:38 -0500

    working as I want

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {TableModule} from "./table/table.module";
+import { MaterialModule } from "./shared/modules/material/material.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TableModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
